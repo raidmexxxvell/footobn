0bfcd556ea530c3ad499d9caabe0858766e7c447
raidmexxxvell <raidmexxxvell@gmail.com>
2025-08-14 10:26:20 +0300

    Add files via upload

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  // publicDir по умолчанию — 'public', так что можно даже не указывать,
+  // но я оставлю для наглядности
+  publicDir: 'public',
+  root: '.', // корень проекта (там, где index.html)
+  build: {
+    outDir: 'dist', // куда складывать билд
+    emptyOutDir: true
+  }
 });
